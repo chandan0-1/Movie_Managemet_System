f5346ce84cd35378755484efcd14144176512224
chandan0-1 <chandanofficial.cn@gmail.com>
2021-05-23 23:05:49 +0530

    Added Components with design

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <div className="search-container">
+          <input />
+          <button id="search-btn">Search</button>
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+export default NavBar;
